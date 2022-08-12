8f123287d4b6a987c81e13181debbb8292b7da99
Varun <varunsesh@gmail.com>
2022-08-12 19:26:44 +0530

    Refactored code + added paddle

--- main.js
+++ main.js
@@ -1,53 +1,92 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
-// ctx.beginPath();
-// ctx.rect(20,40,50,50);
-// ctx.fillStyle="#FF0000";
-// ctx.fill();
-// ctx.closePath();
-
-// ctx.beginPath();
-// ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-// ctx.fillStyle = "green";
-// ctx.fill();
-// ctx.closePath();
-
-// ctx.beginPath();
-// ctx.rect(160,10,100,40);
-// ctx.strokeStyle = "rgba(0, 0, 255, 1.0)";
-// ctx.stroke();
-// ctx.closePath();
 
 let x = canvas.width/2;
 let y =canvas.height-30;
 let dx = 2;
 let dy = -2;
 
-function drawball(x, y, r, cut){
-    ctx.beginPath();
-    ctx.arc(x, y, r, cut, Math.PI*2, false);
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath();
+let rightKeyPressed = false;    
+let leftKeyPressed = false;
+
+
+const ball = {
+    r:10,
+    collFlag:false,
+    draw:function(x,y,cut){
+        ctx.beginPath();
+        ctx.arc(x, y, this.r, cut, Math.PI*2, false);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();    
+    },
+    
+};
+
+const paddle = {
+    paddleWidth:75, 
+    paddleHeight:10,
+    paddleX:canvas.width/2,
+    draw:function(){
+        ctx.beginPath();
+        if(leftKeyPressed){
+            this.paddleX = Math.max(this.paddleX-7, 0);
+        }
+        else if(rightKeyPressed){
+            this.paddleX = Math.min(this.paddleX+7, canvas.width-this.paddleWidth);
+        }    
+        ctx.rect(this.paddleX, canvas.height-this.paddleHeight, this.paddleWidth, this.paddleHeight);
+        ctx.fillStyle="red";
+        ctx.fill();
+        ctx.closePath();
+    },
+    collFlag:false,
+};
+
+
+function keyDownHandler(e){
+    if(e.key==="Right" || e.key==="ArrowRight"){
+        rightKeyPressed = true;
+    }
+    else if(e.key==="Left" || e.key === "ArrowLeft"){
+        leftKeyPressed = true;
+    }
+
+}
+
+function keyUpHandler(e){
+    if(e.key==="Right" || e.key==="ArrowRight" || paddle.paddleX >canvas.width){
+        rightKeyPressed = false;
+    }
+    else if(e.key==="Left" || e.key === "ArrowLeft" || paddle.paddleX<0){
+        leftKeyPressed = false;
+    }
+
 }
 
 //Collision Detection
 function detectCollisions(x_ball, y_ball){
-    if (y_ball+dy < 0 || y_ball - dy >canvas.height){
+    if (y_ball+dy <= ball.r || y_ball + dy >canvas.height-ball.r){
         dy = -dy;
     }
 
-    if (x_ball-dx < 0 || x_ball + dx >canvas.width){
+    if (x_ball+dx <= ball.r || x_ball + dx >canvas.width-ball.r){
         dx = -dx;
     }
+    
 }
+
 
 
 function draw(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawball(x, y, 10, 0);
+    ball.draw(x,y,0);
+    paddle.draw();    
     detectCollisions(x,y);
     x+=dx;
     y+=dy;
 }
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
 setInterval(draw, 10);
